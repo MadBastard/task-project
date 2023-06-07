b0f7125b97b04be0be37b5a4a240abb811962d75
MadBastard <vivivilka16@gmail.com>
2023-06-07 20:49:48 +0300

    Aritmetic-problem

---
+++ src/main/java/chapter2/Arithmetic.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
