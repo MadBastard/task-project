d493bd45ef782c6ac3f5361f55368fb1707807b6
MadBastard <vivivilka16@gmail.com>
2023-06-07 21:55:24 +0300

    ArithmeticSmallestLargest

---
+++ src/main/java/chapter2/two17/ArithmeticSmallestLargest.java
@@ -0,0 +1,33 @@
+package chapter2.two17;
+
+import java.util.Scanner;
+
+public class ArithmeticSmallestLargest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println("Sum:" + (a+b+c));
+        System.out.println("Average:" + ((a*b*c)/3));
+        System.out.println("Product:" + (a*b*c));
+        if (a>b && a>c){
+            System.out.println("These numbers is larger:" + a);
+        }
+        if (b>a && b>c){
+            System.out.println("These numbers is larger:" + b);
+        }
+        if (c>a && c>b){
+            System.out.println("These numbers is larger:" + c);
+        }
+        if (a<b && a<c){
+            System.out.println("These numbers is smallest:" + a);
+        }
+        if (b<a && b<c){
+            System.out.println("These numbers is smallest:" + b);
+        }
+        if (c<a && c<b){
+            System.out.println("These numbers is smallest:" + c);
+        }
+    }
+}
